0c3a1f990631d60cef162119afaa10d5df6d2b09
mdrumon42243 <mdrumonkhan48519@gmail.com>
2022-04-04 15:29:28 +0600

    BarChart and AreaChart displaying done

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import './DashBoard.css'
 
 const DashBoard = () => {
     const [chartData, setChartData] = useState([]);
@@ -12,14 +13,48 @@ const DashBoard = () => {
 
     return (
 
-        <LineChart width={600} height={400} data={chartData}>
-            <Line dataKey={'sell'}></Line>
-            <XAxis dataKey={'month'}></XAxis>
-            <Tooltip></Tooltip>
-            <YAxis></YAxis>
+        <div className="chart-container">
+            <h2 className='chart-title' >
+                <span className='invest'>Invest</span> <span className='vs'>vs</span> <span className='revenue'>Revenue</span>
 
-        </LineChart>
+            </h2>
+            <AreaChart className='area-chart' width={730} height={250} data={chartData}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="month" />
+                <YAxis dataKey='investment' />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="sell" stroke="#44ff00" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            </AreaChart>
 
+            <h2 className='chart-title' >
+                <span className='invest'>Invest</span> <span className='vs'>vs</span> <span className='revenue'>Revenue</span>
+
+            </h2>
+
+            <BarChart className='bar-chart' width={730} height={250} data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" fill="#8884d8" />
+                <Bar dataKey="sell" fill="#44ff00" />
+                <Bar dataKey="revenue" fill="#82ca9d" />
+            </BarChart>
+        </div>
     );
 };
 
